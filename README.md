894a1c21b599f449fb90ee7d04f9daae0c5d4e5a
chiragp2e <Chirag.Garg@mai.io>
2024-12-10 17:33:35 +0530

    updated the contract

--- README.md
+++
@@ -1,1 +0,0 @@
-Gini Vesting Contract
--- vesting/constants.go
+++ vesting/constants.go
@@ -3,14 +3,17 @@ package vesting
 type TokenAllocation int
 
 const (
-	kalpFoundation                 = "0b87970433b22494faff1cc7a819e71bddc7880c"
+	// kalpFoundation                 = "0b87970433b22494faff1cc7a819e71bddc7880c"
+	kalpFoundation                 = "user1"
 	kalpFoundationTotalAllocations = "560000000000000000000000000"
 	kalpFoundationClaimedAmount    = "11200000000000000000000000"
 	kalpFoundationBeneficiaryKey   = "beneficiaries_EcosystemReserve_kalp_foundation"
 	kalpFoundationUserVestingKey   = "uservestings_kalp_foundation"
 	contractAddressRegex           = `^klp.*cc$`
 	hexAddressRegex                = `^0x[0-9a-fA-F]{40}$`
+)
 
+const (
 	Team TokenAllocation = iota
 	Foundation
 	AngelRound
